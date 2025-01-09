27c7e564e29961334cd17d65b4eb63336e4f9dfe
DENG <dyccc01@gmail.com>
2025-01-09 16:08:47 +0800

    update 開始頁面、頁首、頁尾

--- Front-End/src/App.js
+++ Front-End/src/App.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
+import TagsCloud from "./layout/TagsCloud";
 
 const App = () => {
   return (
     <div>
-      <h1>Hello World</h1>
+      <TagsCloud />
     </div>
   );
 };
